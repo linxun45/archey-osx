7e00717ed7cf6d1ea54ca144e2a93e439fc1ff17
athlonreg <15563836030@163.com>
2018-05-14 17:27:37 +0800

    Fixed permission

--- bin/archey_install.sh
+++ bin/archey_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd && git clone https://github.com/athlonreg/archey-osx /usr/local/archey-osx 
+cd && sudo git clone https://github.com/athlonreg/archey-osx /usr/local/archey-osx 
 sudo ln -s /usr/local/archey-osx/bin/archey-osx /usr/bin/archey 
 echo archey >> ./.zshrc && echo archey >> ./.bashrc 
