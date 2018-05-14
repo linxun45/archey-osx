2d18dfe3ed139f24e4f12fae748963de3e136844
athlonreg <15563836030@163.com>
2018-05-14 17:20:07 +0800

    Fixed path

--- bin/archey_install.sh
+++ bin/archey_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd && git clone https://github.com/athlonreg/archey-osx /usr/local/archey-osx/ 
+cd && git clone https://github.com/athlonreg/archey-osx /usr/local/ 
 sudo ln -s /usr/local/archey-osx/bin/archey /usr/bin/archey 
 echo archey >> ./.zshrc && echo archey >> ./.bashrc
