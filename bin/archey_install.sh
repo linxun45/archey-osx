46e4e09c74f57616e10a19f4c26378196c81ffa9
athlonreg <15563836030@163.com>
2018-05-18 19:26:58 +0800

    Fixed a bug about source bashrc

--- bin/archey_install.sh
+++ bin/archey_install.sh
@@ -4,6 +4,6 @@ cd && git clone https://github.com/athlonreg/archey-osx
 sudo mv archey-osx/ /usr/local/ 
 sudo ln -s /usr/local/archey-osx/bin/archey /usr/local/bin/archey 
 echo archey >> ./.zshrc && echo archey >> ./.bashrc 
-echo "[[ -s ~/.bashrc ]] && source ~/.bash" >> ./.bash_profile 
+echo "[[ -s ~/.bashrc ]] && source ~/.bashrc" >> ./.bash_profile 
 source ./.bashrc 
 source ./.zshrc 
